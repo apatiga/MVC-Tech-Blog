e8d70398af931a5320f0f6238f1292588d02e740
apatiga <128877342+apatiga@users.noreply.github.com>
2023-07-13 18:10:36 -0700

    fixed issue with running npm and sql password in connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,14 @@
 const Sequelize = require('sequelize');
 require('dotenv').config(); // Import sensitive data from .env
 
+console.log(process.env.DB_PASSWORD)
+
 const sequelize = process.env.JAWSDB_URL 
   ? new Sequelize(process.env.JAWSDB_URL) // JAWSDB_URL is for HEROKU
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, { // This is for Local
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, null, { // This is for Local
       host: '127.0.0.1',
       dialect: 'mysql',
-      dialectOptions: {
-        decimalNumbers: true,
-      },
+      port: 3306,
     });
 
 module.exports = sequelize;
